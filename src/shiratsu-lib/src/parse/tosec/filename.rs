2c6e209ce426e7a00467babc5d379e061c8493f3
Ronny Chan <ronny6993@gmail.com>
2020-06-17 19:44:49 -0400

    TOSEC: Allow wobbly exceptions to the naming convention

--- src/shiratsu-lib/src/parse/tosec/filename.rs
+++ src/shiratsu-lib/src/parse/tosec/filename.rs
@@ -8,7 +8,6 @@ use nom::{
     bytes::complete::is_not,
     // see the "streaming/complete" paragraph lower for an explanation of these submodules
     character::complete::char,
-    combinator::complete,
     multi::many0,
     sequence::delimited,
     IResult,
@@ -25,18 +24,21 @@ fn brackets(input: &str) -> IResult<&str, &str> {
 pub fn do_parse<'a, 'b>(title: &'a str, input: &'b str) -> IResult<&'b str, NameInfo> {
     lazy_static! {
         static ref DATE: Regex = Regex::new(
-            r"^((19|20)[\dx]{2})$|^((19|20)[\dx]{2})-[\d]{2}$|^((19|20)[\dx]{2})-[\d]{2}-[\d][\dx]$"
+            r"^((19|20)[\dxX]{2})$|^((19|20)[\dxX]{2})-[\d]{2}$|^((19|20)[\dxX]{2})-[\d]{2}-[\d][\dxX]$"
         )
         .unwrap();
         static ref EXTRACT_PART: Regex = Regex::new(r"^\d+").unwrap();
-        static ref EXTRACT_DEMO: Regex = Regex::new(r" \(demo\)").unwrap();
+        static ref EXTRACT_DEMO: Regex = Regex::new(r"\s?\(demo(-playable|-kiosk|-rolling|-slideshow)?\)").unwrap();
         static ref REVISION: Regex = Regex::new(r"Rev ([0-9]+)$").unwrap();
         static ref VERSION: Regex = Regex::new(r"v([\.0-9]+)$").unwrap();
     }
     let mut title = title;
+    let (input, _) = many0(char(' '))(input)?;
     let (input, _publisher) = strict_parens(input)?;
     let (input, tags) = many0(strict_parens)(input)?;
-    let (input, flags) = complete(many0(brackets))(input)?;
+    // TOSEC is wobbly
+    let (input, _) = many0(char(' '))(input)?;
+    let (input, flags) = many0(brackets)(input)?;
     let mut part_number: Option<i32> = None;
     let mut version: Option<String> = None;
     let mut is_unlicensed = false;
@@ -107,12 +109,19 @@ pub fn do_parse<'a, 'b>(title: &'a str, input: &'b str) -> IResult<&'b str, Name
 
 fn tosec_parser<'a>(input: &str) -> Result<NameInfo> {
     lazy_static! {
-        static ref FIND_TITLE_WITH_DEMO_AND_DATE: Regex = Regex::new(r"^(.+) (\(((19|20)[\dx]{2})\)|\(((19|20)[0-9x]{2})-[0-9]{2}\)|\(((19|20)[0-9x]{2})-[0-9]{2}-[0-9][0-9x]\))").unwrap();
+        // TOSEC lies in their convention.
+        // this regex matches happy path, and then
+        // also matches a sad path where everything
+        // goes out the window and we just match up
+        // to the first parenthesis.
+        static ref FIND_TITLE_WITH_DEMO_AND_DATE: fancy_regex::Regex =
+            fancy_regex::Regex::new(r"^(.+)(\s?(\(demo(-playable|-kiosk|-rolling|-slideshow)?\))?(\(((19|20)[\dxX]{2})\)|\(((19|20)[0-9xX]{2})-[0-9]{2}\)|\(((19|20)[0-9xX]{2})-[0-9]{2}-[0-9][0-9xX]\))|(?=\s\())").unwrap();
     };
-
     let title_captures =
         FIND_TITLE_WITH_DEMO_AND_DATE
             .captures(input)
+            .ok()
+            .flatten()
             .ok_or(ParseError::BadFileNameError(
                 NamingConvention::TOSEC,
                 String::from(input),
@@ -128,14 +137,9 @@ fn tosec_parser<'a>(input: &str) -> Result<NameInfo> {
             String::from(input),
         ))?
         .as_str();
-    let _ = title_captures.get(1).ok_or(ParseError::BadFileNameError(
-        NamingConvention::TOSEC,
-        String::from(input),
-    ))?;
     let value = do_parse(title, &input[full_match.end()..])
         .map(|(_, value)| value)
         .map_err(|_| ParseError::BadFileNameError(NamingConvention::TOSEC, String::from(input)))?;
-
     Ok(value)
 }
 
