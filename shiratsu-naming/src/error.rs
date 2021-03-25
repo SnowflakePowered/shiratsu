975e20e148ac4c381adf380fba1909bd2effa71a
Ronny Chan <ronny6993@gmail.com>
2021-03-25 16:45:24 -0400

    naming: remove unused parse error

--- shiratsu-naming/src/error.rs
+++ shiratsu-naming/src/error.rs
@@ -7,7 +7,6 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum NameError {
-    ParseError(String),
     BadFileNameError(NamingConvention, String),
     RegionError(RegionError),
 }
@@ -23,7 +22,6 @@ impl Error for NameError {}
 impl Display for NameError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            NameError::ParseError(val) => write!(f, "{}", val),
             NameError::BadFileNameError(convention, string) =>
                 write!(f, "The \"{}\" could not be parsed properly in the {:?} naming convention", string, convention),
             NameError::RegionError(region_err) =>
